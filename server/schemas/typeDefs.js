8ba3f983ebd1444f25895825b29dd93f48e3aee1
Brandon Bloodsworth <brandon1bloodsworth@gmail.com>
2024-04-01 19:45:16 -0400

    Defined type Student and Project

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,23 @@
 const typeDefs = `
+type Student {
+  id: ID!
+  firstName: String!
+  lastName: String!
+  email: String!
+  password: String!
+  openEmploy: Boolean!
+  projects: [Project]
+}
+
+type Project {
+  id: ID!
+  name: String!
+  baseLanguage: String!
+  createdAt: Date!
+  openCollab: Boolean!
+  student: Student
+}
+
   type Query {
   }
 
