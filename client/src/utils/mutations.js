039027b32452bd42dcdb7a7fbe9ea7390c3e1125
Brandon Bloodsworth <brandon1bloodsworth@gmail.com>
2024-04-02 21:42:01 -0400

    Fixed bug with mutations and SignupPage.jsx

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,31 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
-        _id
+      student {
+        id
       }
     }
   }
 `;
 
 export const ADD_STUDENT = gql`
-mutation Mutation($firstName: String!, $lastName: String!, $email: String!, $password: String!, $openEmploy: Boolean) {
-    addStudent(firstName: $firstName, lastName: $lastName, email: $email, password: $password, openEmploy: $openEmploy) {
+  mutation Mutation(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+    $openEmploy: Boolean
+  ) {
+    addStudent(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+      openEmploy: $openEmploy
+    ) {
       token
       student {
         id
