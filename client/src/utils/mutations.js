b10dc0e6c99944fbeb847953a407bfae9c6de659
Christopher <christopher.ferraro34@gmail.com>
2024-04-04 17:07:07 -0400

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,7 +4,7 @@ export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      student {
+      user {
         _id
       }
     }
